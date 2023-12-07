82695361cf899277ae4ee1800456de1abcc8e0cf
iammarcode <26892819+iammarcode@users.noreply.github.com>
2023-12-07 23:44:53 +0800

    Remove Duplicates Most Twice

---
+++ Array/RemoveDuplicatesMostTwice.java
@@ -0,0 +1,34 @@
+package Array;
+
+public class RemoveDuplicatesMostTwice {
+    // My solution
+    public int solution1(int[] nums) {
+        int k = 0;
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[k] != nums[i]) {
+                nums[++k] = nums[i];
+                count = 1;
+            } else if (count < 2) {
+                nums[++k] = nums[i];
+                count++;
+            }
+        }
+
+        return k + 1;
+    }
+
+    // Best solution
+    public int solution2(int[] nums) {
+        int k = 0;
+
+        for (int n : nums) {
+            if (k < 2 || n != nums[k - 2]) {
+                nums[k++] = n;
+            }
+        }
+
+        return k;
+    }
+}
